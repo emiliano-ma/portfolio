059e36ebf5a819f8cec2680cc77523266aac9f4f
emiliano-ma <emilianomainero@gmail.com>
2020-09-02 16:06:47 +0200

    adding the meta tag to the index.html, error persists

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const HelloWorld = () => {
-  return <h2>Hello World!</h2>;
+  return <h2>Hello World</h2>;
 };
 
-ReactDOM.render(<HelloWorld/>, document.getElementById("app"));
+ReactDOM.render(<HelloWorld />, document.getElementById("app"));
